b070cd28d6e9b0478bfb12778c64c04bbfa3208a
Alex Kocharin <alex@kocharin.ru>
2017-01-04 12:11:10 +0000

    Fix config processing

---
+++ internal/search/reindex/tasklist.js
@@ -0,0 +1,9 @@
+// List of tasks that should be started when running reindex from ACP
+//
+
+'use strict';
+
+
+module.exports = function (N, apiPath) {
+  // nothing to do here, the list is filled in elsewhere (e.g. in forum hooks)
+};
