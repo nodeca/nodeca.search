c058a26a76842473ec58eec6aa00dda295956886
Alex Kocharin <alex@kocharin.ru>
2017-01-27 18:02:09 +0000

    Move query builder to nodeca.search

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ exports.name = 'nodeca.search';
 exports.init = function (N) { require('./lib/autoload.js')(N); };
 
 exports.server = require('./lib/server');
+exports.escape = require('./lib/escape');
