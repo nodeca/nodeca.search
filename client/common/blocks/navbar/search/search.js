c4e9185f63a988ca1d1f6a9d41f6316500644402
Alex Kocharin <alex@kocharin.ru>
2017-02-28 13:38:36 +0000

    Hide search bar when user presses ESC or clicks anywhere

--- client/common/blocks/navbar/search/search.js
+++ client/common/blocks/navbar/search/search.js
@@ -6,19 +6,62 @@
 const _  = require('lodash');
 
 
-N.wire.on(module.apiPath + ':show', function show_search_bar(data) {
-  let container = data.$this.closest('.navbar');
+// Hide search bar if user presses Escape key,
+// and focus is inside search bar
+//
+function keydown_handler(event) {
+  if (event.which !== 27 /* ESC */) return;
 
-  container.addClass('nav-search-on');
-  container.find('.nav-search__input').focus();
-});
+  event.preventDefault();
+  event.stopPropagation();
+
+  /* eslint-disable no-use-before-define */
+  hide_search_bar();
+}
 
 
-N.wire.on(module.apiPath + ':hide', function hide_search_bar(data) {
-  let container = data.$this.closest('.navbar');
+// Click inside form, need to stop propagation of this event
+// to global handlers
+//
+function click_inside_form_handler(event) {
+  event.stopPropagation();
+}
 
-  container.removeClass('nav-search-on');
-});
+
+// Hide search bar if user clicks anywhere outside of it
+//
+function click_outside_form_handler(event) {
+  if (event.which === 3 /* right mouse button */) return;
+
+  hide_search_bar();
+}
+
+
+// Show search bar
+//
+function show_search_bar() {
+  $('.navbar').addClass('nav-search-on');
+  $('.nav-search__input').focus();
+
+  $(document).on('keydown', '.nav-search__form', keydown_handler);
+  $(document).on('click focusin', '.nav-search__form', click_inside_form_handler);
+  $(document).on('click focusin', click_outside_form_handler);
+}
+
+
+// Hide search bar
+//
+function hide_search_bar() {
+  $('.navbar').removeClass('nav-search-on');
+
+  $(document).off('keydown', '.nav-search__form', keydown_handler);
+  $(document).off('click focusin', '.nav-search__form', click_inside_form_handler);
+  $(document).off('click focusin', click_outside_form_handler);
+}
+
+
+N.wire.on(module.apiPath + ':show', show_search_bar);
+N.wire.on(module.apiPath + ':hide', hide_search_bar);
 
 
 N.wire.on(module.apiPath + ':change_area', function set_search_area(data) {
@@ -31,6 +74,9 @@ N.wire.on(module.apiPath + ':change_area', function set_search_area(data) {
 
 
 N.wire.on(module.apiPath + ':submit', function submit_search(data) {
+  // remove click and keydown listeners
+  hide_search_bar();
+
   let select = data.$this.find('.nav-search__select');
   let params = _.assign({}, select.data('params'));
   let apiPath = select.data('method') || 'search.general';
